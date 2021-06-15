237ca3f618bfe25eafeac5f4431f5406ecb76001
Willem van Heemstra <wvanheemstra@icloud.com>
2021-06-15 09:16:22 +0000

    Add containers/docker-coins/worker/worker.py

---
+++ containers/docker-coins/worker/worker.py
@@ -0,0 +1,16 @@
+import logging
+import os
+from redis import Redis
+import requests
+import time
+
+DEBUG = os.environ.get("DEBUG", "").lower().startswith("y")
+
+log = logging.getLogger(__name__)
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+redis = Redis("redis")
