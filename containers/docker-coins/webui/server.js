415435a781f682538aa7656774e6baad9e6b60b3
Willem van Heemstra <willem@vanheemstrasystems.com>
2021-06-28 15:09:19 +0200

    Update server.js

--- containers/docker-coins/webui/server.js
+++ containers/docker-coins/webui/server.js
@@ -11,6 +11,19 @@ app.get('/', function (req, res) {
     res.redirect('/index.html');
 });
 
+app.get('/json', function (req, res) {
+    client.hlen('wallet', function (err, coins) {
+        client.get('hashes', function (err, hashes) {
+            var now = Date.now() / 1000;
+            res.json( {
+                coins: coins,
+                hashes: hashes,
+                now: now
+            });
+        });
+    });
+});
+
 app.use(express.static('static'));
 
 var server = app.listen(80, function () {
